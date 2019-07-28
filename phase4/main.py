c8e3eb55a5c9bc2e34d34737ba3fc77c46550cba
loveyouforever04 <1691049547@qqcom>
2019-07-28 17:53:27 +0800

    xfy0728

---
+++ phase4/main.py
@@ -0,0 +1,11 @@
+from person_chinese import Person,Chinese
+
+
+print('-----cutting line-----')
+man = Person()
+man.talk()
+
+print('-----cutting line-----')
+chinese_man = Chinese()
+chinese_man.talk()
+chinese_man.walk()
---
+++ phase4/person_chinese.py
@@ -0,0 +1,13 @@
+class Person():
+    '''父类 - 人'''
+
+    def talk(self):
+        print("person is talking...")
+        return
+
+class Chinese(Person):
+    '''子类 - 人'''
+
+    def walk(self):
+        print('A Chinese is walking...')
+        return
